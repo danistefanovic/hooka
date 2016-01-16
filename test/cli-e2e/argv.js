920dbd89762f3388687be6086977f0c267bcff12
Daniel Stefanovic <daniel.stefanovic@gmail.com>
2016-01-16 18:03:55 +0100

    Increment port for each e2e test

--- test/cli-e2e/argv.js
+++ test/cli-e2e/argv.js
@@ -4,9 +4,14 @@ import cli from '../helpers/cli';
 
 const defaultPort = 3000;
 const testFolder = path.join(__dirname, '../');
+let port = defaultPort + 1;
 
 describe('CLI argv', () => {
     let child = null;
+
+    beforeEach(() => {
+        port++;
+    });
 
     afterEach(() => {
         child.kill();
@@ -19,26 +24,26 @@ describe('CLI argv', () => {
         });
 
         it('should run on the provided port', (callback) => {
-            child = cli(['--port', 1337]);
-            serverReady(1337, callback);
+            child = cli(['--port', port]);
+            serverReady(port, callback);
         });
 
         it('should run on the provided port (alias)', (callback) => {
-            child = cli(['-p', 4242]);
-            serverReady(4242, callback);
+            child = cli(['-p', port]);
+            serverReady(port, callback);
         });
     });
 
     describe('config file', () => {
         it('should start if a webhooks.json file is in the CWD', (callback) => {
-            child = cli();
-            serverReady(defaultPort, callback);
+            child = cli(['--port', port]);
+            serverReady(port, callback);
         });
 
         it('should throw an error if no webhooks.json is found in the CWD', (callback) => {
             const handleServerReady = jasmine.createSpy('handleServerReady');
-            child = cli(null, '../../');
-            serverReady(defaultPort, handleServerReady);
+            child = cli(['--port', port], '../../');
+            serverReady(port, handleServerReady);
             child.on('close', (code) => {
                 expect(code).toBe(1);
                 expect(handleServerReady).not.toHaveBeenCalled();
@@ -47,19 +52,19 @@ describe('CLI argv', () => {
         });
 
         it('should start if the provided config file is available', (callback) => {
-            child = cli(['--config', path.join(testFolder, 'webhooks.json')]);
-            serverReady(defaultPort, callback);
+            child = cli(['--port', port, '--config', path.join(testFolder, 'webhooks.json')]);
+            serverReady(port, callback);
         });
 
         it('should start if the provided config file is available (alias)', (callback) => {
-            child = cli(['-c', path.join(testFolder, 'webhooks.json')]);
-            serverReady(defaultPort, callback);
+            child = cli(['--port', port, '-c', path.join(testFolder, 'webhooks.json')]);
+            serverReady(port, callback);
         });
 
         it('should throw an error if the provided config file is not available', (callback) => {
             const handleServerReady = jasmine.createSpy('handleServerReady');
-            child = cli(['--config', path.join(testFolder, 'nope.json')]);
-            serverReady(defaultPort, handleServerReady);
+            child = cli(['--port', port, '--config', path.join(testFolder, 'nope.json')]);
+            serverReady(port, handleServerReady);
             child.on('close', (code) => {
                 expect(code).toBe(1);
                 expect(handleServerReady).not.toHaveBeenCalled();
