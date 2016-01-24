03c106adb8faaecef37ad6d8771c7bc1239a1562
Daniel Stefanovic <daniel.stefanovic@gmail.com>
2016-01-24 20:10:21 +0100

    Add JSON bodyparser #11

--- src/server/routes/registerMiddleware.js
+++ src/server/routes/registerMiddleware.js
@@ -4,6 +4,7 @@ import secretToken from './secretTokenMiddleware';
 export default function registerMiddleware({ router, secret }) {
     if (!router) throw new Error('No router provided');
 
+    router.use(bodyParser.json());
     router.use(bodyParser.urlencoded({ extended: true }));
     router.use(secretToken(secret));
 }
