1310dc75ee38ae48068f48e89bca065e3aca35bd
Daniel Stefanovic <daniel.stefanovic@gmail.com>
2016-02-06 13:21:20 +0100

    Add middleware function comments #18

--- src/server/middleware/parseJsonValidator.js
+++ src/server/middleware/parseJsonValidator.js
@@ -1,6 +1,12 @@
 import areObjectValuesDefined from '../utils/areObjectValuesDefined';
 import pickVariablesFromJson from '../utils/pickVariablesFromJson';
 
+/**
+ * "parseJson" Express middelware
+ * @see https://github.com/danistefanovic/hooka/blob/master/docs/webhooks.md#parsejson
+ * @param {Object} parseJson Config for "parseJson"
+ * @return {void}
+ */
 export default function parseJsonValidator(parseJson) {
     return (req, res, next) => {
         const env = pickVariablesFromJson(req.body, parseJson);
--- src/server/middleware/ruleValidator.js
+++ src/server/middleware/ruleValidator.js
@@ -1,5 +1,11 @@
 import isRequestValid from '../validation/isRequestValid';
 
+/**
+ * Rule validator Express middleware
+ * @see https://github.com/danistefanovic/hooka/blob/master/docs/webhooks.md#validate
+ * @param {Array} validationRules Validation rules
+ * @return {void}
+ */
 export default function ruleValidator(validationRules) {
     return (req, res, next) => {
         if (validationRules && !isRequestValid(req, validationRules)) {
